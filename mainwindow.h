98d77381a691734855cb137f78d6497bdf54b45b
yang-quan <yang-quan@neusoft.com>
2017-12-13 19:30:21 +0800

    releaseCI

--- mainwindow.h
+++ mainwindow.h
@@ -2,26 +2,6 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-/*
-namespace Ui {
-class MainWindow;
-
-}
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-
-public:
-    explicit MainWindow(QWidget *parent = 0);
-    ~MainWindow();
-
-private:
-    Ui::MainWindow *ui;
-    //Ui::ArrowWidget *arrowWidget;
-};
-*/
-//
 class ArrowWidget;
 class ArrowWidget : public QWidget
 {
