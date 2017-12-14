b97416be570ee58f8981f84e0b314e0e524ee065
yang-quan <yang-quan@neusoft.com>
2017-12-14 19:13:15 +0800

    last vision

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,10 +3,14 @@
 #include <QPainter>
 #include <QFont>
 #include <QLayout>
+#include <QTextCodec>
 
 ArrowWidget::ArrowWidget(QWidget *parent)
     : QWidget(parent)
 {
+    this->setWindowFlags(Qt::FramelessWindowHint);//去掉标题栏
+    this->showMaximized();
+    QTextCodec::setCodecForLocale  (QTextCodec::codecForName("utf8"));//设置编码格式
     resize(700,300);//设置窗口大小
 }
 
@@ -14,7 +18,7 @@ void ArrowWidget::paintEvent(QPaintEvent *event)
 {
 
         int i = 100,j = 180; //str位置
-        QString str("Howard");
+        QString str("ABCDE");
         QPainterPath path;
         QPen pen(Qt::black);//轮廓为黑色的
         QPainter painter(this);
@@ -22,7 +26,9 @@ void ArrowWidget::paintEvent(QPaintEvent *event)
         pen.setStyle(Qt::SolidLine);//轮廓线为实线
         pen.setWidth(3);//设置字体轮廓线条粗细
         QFont font("Times New Roman", 100);//设置字体样式
-        
+        font.setLetterSpacing(QFont::PercentageSpacing,-2); //设置字间距 默认100%
+        font.setLetterSpacing(QFont::AbsoluteSpacing,-4); //默认1
+
         font.setBold(true);//设置粗体
         path.addText(i, j, font, str);//add path
         painter.strokePath(path, pen);//set SolidLine style
